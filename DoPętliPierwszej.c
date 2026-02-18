a8431e479516500e4ead9dbedc7975fe1d3d2a4d
Artix65 <98666171+Artix65@users.noreply.github.com>
2026-02-18 19:59:34 +0100

    Update DoPętliPierwszej.c
    Szybka poprawka

--- DoPętliPierwszej.c
+++ DoPętliPierwszej.c
@@ -1,3 +1,4 @@
+/* USER CODE BEGIN 2 */
   HAL_TIM_Base_Start(&htim1);
   RS485_WlaczOdbieranie();
   
@@ -5,7 +6,7 @@
   DanePomiarowe obce_pomiary;
   uint8_t bufor_nadawczy[10];
   uint8_t bufor_odbiorczy[2];
-
+  /* USER CODE END 2 */
 
   while (1)
   {
@@ -14,12 +15,10 @@
           moje_pomiary.temperatura = 0;
           moje_pomiary.wilgotnosc = 0;
       }
-
       RS485_WlaczNadawanie();
       uint8_t zapytanie = 'Q'; //Q jako Query
       HAL_UART_Transmit(&huart1, &zapytanie, 1, 100);
-      RS485_WlaczOdbieranie(); //powrót do nasłuchu
-
+      RS485_WlaczOdbieranie(); //Powrót do nasłuchu
       if(HAL_UART_Receive(&huart1, bufor_odbiorczy, 2, 100) == HAL_OK) {
           obce_pomiary.wilgotnosc = bufor_odbiorczy[0];
           obce_pomiary.temperatura = bufor_odbiorczy[1];
@@ -27,5 +26,5 @@
           obce_pomiary.wilgotnosc = 0xFF;
           obce_pomiary.temperatura = 0xFF;
       }
-      HAL_Delay(2000); //Dla DHT11
+      HAL_Delay(2000); //dla DHT11
   }
